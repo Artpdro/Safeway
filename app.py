bca08c2f242e042d421148aeb4e05a4eb11f17d0
Arthur Pedro <arthurpedro.profissional@gmail.com>
2025-09-16 16:47:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ DOCUMENTATION_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Safeway - Documentação</title>
+    <title>Safeway - Documentation</title>
     <style>
         * {
             margin: 0;
